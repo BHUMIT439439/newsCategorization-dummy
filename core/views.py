11a1df87ade4cc3f7c626ca9a152b4c241495913
Ronak Padaliya <ronakpadaliya77@gmail.com>
2022-02-25 11:37:50 +0530

    Style added

--- core/views.py
+++ core/views.py
@@ -4,23 +4,80 @@ from django.views.decorators.http import require_http_methods
 from sklearn.feature_extraction.text import CountVectorizer
 import pandas as pd
 
+from joblib import dump, load
 import joblib
+import re
+import nltk
+# nltk.download()
+from nltk.corpus import stopwords
+# nltk.download('stopwords')
+from nltk.tokenize import word_tokenize
+# nltk.download('punkt')
+from nltk.stem import WordNetLemmatizer
+# nltk.download('wordnet')
 
+print("-------------------------------------------------------------")
+dataset = pd.read_csv('./NewsCategorizer.csv', sep=",", encoding='utf-8')
+
+dataset['Category'] = dataset['category']
+dataset.drop('category',axis='columns', inplace=True)
+
+dataset['Text'] = dataset['headline'] + dataset['short_description']
+dataset.drop('headline',axis='columns', inplace=True)
+dataset.drop('short_description',axis='columns', inplace=True)
+
+dataset.drop('links',axis='columns', inplace=True)
+dataset.drop('keywords',axis='columns', inplace=True)
+dataset['CategoryId'] = dataset['Category'].factorize()[0]
+
+def remove_tags(text):
+    remove = re.compile(r'')
+    return re.sub(remove, '', text)
+    
+dataset['Text'] = dataset['Text'].apply(remove_tags)
+
+def special_char(text):
+    reviews = ''
+    for x in text:
+        if x.isalnum():
+            reviews = reviews + x
+        else:
+            reviews = reviews + ' '
+    return reviews
+dataset['Text'] = dataset['Text'].apply(special_char)
+
+def convert_lower(text):
+    return text.lower()
+dataset['Text'] = dataset['Text'].apply(convert_lower)
+
+def remove_stopwords(text):
+    stop_words = set(stopwords.words('english'))
+    words = word_tokenize(text)
+    return [x for x in words if x not in stop_words]
+
+dataset['Text'] = dataset['Text'].apply(remove_stopwords)
+
+def lemmatize_word(text):
+    wordnet = WordNetLemmatizer()
+    return " ".join([wordnet.lemmatize(word) for word in text])
+
+dataset['Text'] = dataset['Text'].apply(lemmatize_word)
+# dataset['Text'] = dataset['headline'] + dataset['short_description']
+# dataset.head()
+print("-------------------------------------------------------------")
 
 @require_http_methods(['GET', 'POST'])
 def PredictCategory(request):
-    print("-------------------------------------------------------------")
-    dataset = pd.read_csv('./NewsCategorizer.csv', sep=",", encoding='utf-8')
-    dataset['Text'] = dataset['headline'] + dataset['short_description']
-    dataset.head()
-    print("-------------------------------------------------------------")
+    news=""
     if request.method == 'POST':
         cv = CountVectorizer(max_features = 5000)
         cv.fit(dataset.Text)
-        text = cv.transform([request.POST['text']])
+        text = cv.transform([request.POST['mynews']])
+        news=request.POST['mynews']
         model = joblib.load('./oneVsRest.joblib')
         category = model.predict(text)
-        if category == [0]:
+
+        if category == [0]:     
             result = "WellNess News"
         elif category == [1]:
             result = "POLITICS News"
@@ -41,5 +98,5 @@ def PredictCategory(request):
         elif category == [9]:
             result = "SPORTS News"
 
-        return render(request, 'core/form.html', {'category': result})
-    return render(request, 'core/form.html', {'category': ""})
+        return render(request, 'core/form.html', {'category': result,'news':news})
+    return render(request, 'core/form.html', {'category': "", 'news':news})
